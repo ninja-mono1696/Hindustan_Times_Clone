d8546b6cffa0de83ee8acae9dc0a1948a6e374b6
Monojit Mondal <mondalmonojit1696@gmail.com>
2022-06-19 20:29:45 +0530

    homePage added

--- index.js
+++ index.js
@@ -44,3 +44,17 @@ setTimeout("renderTime()", 1000);
 }
 
 renderTime();
+
+
+let y=document.querySelector("#email");
+document.querySelector("#btn").addEventListener("click",subscribe);
+function subscribe(){
+  let y=document.querySelector("#email").value;
+
+  if(y==""){
+  alert("enter email")}
+  else{
+    alert("subscribed succesfully")
+  }
+
+}
